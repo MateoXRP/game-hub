b137045b9cb896683880c80440f54872a2ae6e10
Mathew Safford <matsafford@Mathews-MacBook-Air.local>
2025-06-13 02:25:16 -0700

    Magic Game added

--- src/GameHub.jsx
+++ src/GameHub.jsx
@@ -11,6 +11,12 @@ export default function GameHub() {
 
       <div className="flex flex-col space-y-6 w-full max-w-xs">
         <a
+          href="https://magic-game-silk.vercel.app/"
+          className="bg-gray-800 hover:bg-gray-700 text-white text-xl py-4 px-6 rounded-2xl text-center shadow-lg transition"
+        >
+          🔮 Magic Game
+        </a>
+        <a
           href="https://war-game-chi.vercel.app/"
           className="bg-gray-800 hover:bg-gray-700 text-white text-xl py-4 px-6 rounded-2xl text-center shadow-lg transition"
         >
